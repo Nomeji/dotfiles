42010da70febdf5b41615f39371beec168f043de
Nomeji <nomeji@emersion.fr>
2019-01-03 12:14:48 +0100

    Add support for multi streams (copy every tracks)

--- Documents/Scripts/ffmpeg_10To8.sh
+++ Documents/Scripts/ffmpeg_10To8.sh
@@ -1,9 +1,9 @@
 infolder=$1
 outfolder=$2
 
-cd $infolder
+cd "$infolder"
 
 for i in *.mkv;
-	do ffmpeg -i "$i" -c:v h264 -crf 19 -preset fast -profile:v high -level 4.2 -pix_fmt yuv420p -c:a copy -c:s copy -map_metadata 0 "$outfolder/$i";
+	do ffmpeg -i "$i" -c:v h264 -crf 19 -preset fast -profile:v high -level 4.2 -pix_fmt yuv420p -c:a copy -c:s copy -map 0 -map_metadata 0 "$outfolder/$i";
 	echo "output --> $outfolder/$i";
 done
