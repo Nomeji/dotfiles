14cfcc9e585c274ae54dc71bf4520cd722d670bd
Nomeji <nomeji@emersion.fr>
2021-02-25 14:58:23 +0100

    soundcard-toggle: use card name instead of index

--- .config/i3status-rs/soundcard-name.sh
+++ .config/i3status-rs/soundcard-name.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-default_sink=$(pacmd list-sinks | grep "*" | cut -d " " -f5)
-
-if [[ $default_sink -eq 1 ]]; then
-	echo P780
-else
-	echo Intel
-fi
+echo $(pactl info | grep "Default Sink" | cut -d "." -f4)
