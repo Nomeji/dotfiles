4261f9ded699b3bc439550e70fbddb7bb4301a89
Nomeji <nomeji@emersion.fr>
2023-03-15 17:44:32 +0100

    change audio device switch

--- .config/i3status-rs/soundcard-name.sh
+++ .config/i3status-rs/soundcard-name.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 echo $(pactl info | grep "Default Sink" | cut -d "." -f2)
+
+# name=$(pw-dump | jq -r '.[] | select(.info.props."media.class" == "Audio/Sink") | select(.info.state == "running") | .info.props."node.description"')
+# echo "${name::20}"
